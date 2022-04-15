21c19d385a8e06e747a59266189f343e3a227f23
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-15 17:23:07 -0400

    wip: blog page - read all posts

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,3 +1,35 @@
-export default function BlogPage() {
-  return <div>index</div>;
+import { Meta } from '../../components/layout';
+import { getAllPosts } from '../../utils/postsApi';
+import type { PostType } from '../../types';
+
+type Props = {
+  allPosts: PostType[];
+};
+
+export default function BlogPage({ allPosts }: Props) {
+  return (
+    <>
+      <Meta title='Blog | Cesario Nivar' />
+
+      <div className='page'>
+        {allPosts.map((post) => (
+          <div key={post.slug}>{post.excerpt}</div>
+        ))}
+      </div>
+    </>
+  );
 }
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'coverImage',
+    'excerpt',
+  ]);
+
+  return {
+    props: { allPosts },
+  };
+};
